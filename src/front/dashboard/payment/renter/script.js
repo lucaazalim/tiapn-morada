1b757801034daca53653bbbea150ddeb43d7daaa
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-02 17:12:05 -0300

    Modificações em Payment/renter #39

--- src/front/dashboard/payment/renter/script.js
+++ src/front/dashboard/payment/renter/script.js
@@ -1,25 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const enviarButton = document.querySelector('#enviarButton');
-    const aprovarButton = document.querySelector('#aprovarButton');
-    const rejeitarButton = document.querySelector('#rejeitarButton');
+    const cancelarButton = document.querySelector('#cancelarButton');
 
     if (enviarButton) {
         enviarButton.addEventListener('click', function () {
             alert('Aviso de pagamento do aluguel criado!');
         });
     }
-
-    if (aprovarButton) {
-        aprovarButton.addEventListener('click', function () {
-            alert('Pagamento do aluguel aprovado!');
-        });
-    }
-
-    if (rejeitarButton) {
-        rejeitarButton.addEventListener('click', function () {
-            alert('Pagamento do aluguel rejeitado!');
-        });
-    }
+    
     if (cancelarButton) {
         cancelarButton.addEventListener('click', function () {
             alert('Aviso de pagamento cancelado!');
