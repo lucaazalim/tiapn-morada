cc9ded286b50ea7bada2ccf539663c94b13243ec
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-02 20:57:42 -0300

    Atualização do script.js em payment/owner/ #39

--- src/front/dashboard/payment/owner/script.js
+++ src/front/dashboard/payment/owner/script.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     const aprovarButton = document.querySelector('#aprovarButton');
     const rejeitarButton = document.querySelector('#rejeitarButton');
+    const checkboxes = document.querySelectorAll('.form-check-input');
+
+    function getSelectedApartments() {
+        const selectedApartments = [];
+
+        checkboxes.forEach(checkbox => {
+            if (checkbox.checked) {
+                const cardBody = checkbox.closest('.card-body');
+                const apartmentTitle = cardBody.querySelector('.card-title').textContent;
+                selectedApartments.push(apartmentTitle);
+            }
+        });
+
+        return selectedApartments;
+    }
 
     if (aprovarButton) {
         aprovarButton.addEventListener('click', function () {
-            alert('Pagamento do aluguel aprovado!');
+            const selectedApartments = getSelectedApartments();
+            if (selectedApartments.length > 0) {
+                alert('Pagamento aprovado para os apartamentos: ' + selectedApartments.join(', '));
+            } else {
+                alert('Selecione um apartamento para aprovação.');
+            }
         });
     }
 
