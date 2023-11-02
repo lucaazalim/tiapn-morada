08e0349f8025694d351f9b6ad3ae8e087b9f2bc4
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-02 17:22:53 -0300

    Atualização script.js #39

--- src/front/dashboard/payment/renter/script.js
+++ src/front/dashboard/payment/renter/script.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const enviarButton = document.querySelector('#enviarButton');
     const cancelarButton = document.querySelector('#cancelarButton');
+    const imovelPagamentoSelect = document.querySelector('#ImovelPagamento');
+    const mesPagamentoSelect = document.querySelector('#MesPagamento');
+    const anoPagamentoSelect = document.querySelector('#AnoPagamento');
 
     if (enviarButton) {
         enviarButton.addEventListener('click', function () {
-            alert('Aviso de pagamento do aluguel criado!');
+            const imovelSelecionado = imovelPagamentoSelect.options[imovelPagamentoSelect.selectedIndex].text;
+            const mesPagamento = mesPagamentoSelect.options[mesPagamentoSelect.selectedIndex].text;
+            const anoPagamento = anoPagamentoSelect.value;
+            alert(`Aviso de pagamento do aluguel do imóvel ${imovelSelecionado} para ${mesPagamento} de ${anoPagamento} criado!`);
         });
     }
     
