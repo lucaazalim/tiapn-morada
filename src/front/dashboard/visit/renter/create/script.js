e6123da4287641a8bd35d9ad92073613dce154f4
Kimberly Spencer <kimberly.liz@sga.pucminas.br>
2023-11-22 13:51:28 -0300

    fix #37 #46 #56

--- src/front/dashboard/visit/renter/create/script.js
+++ src/front/dashboard/visit/renter/create/script.js
@@ -1,95 +1,58 @@
-let isEventSelected = false; // Variável para rastrear se um evento foi selecionado
+import * as API from '../../../../assets/script/api.js';
 
-showCalendar();
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
 
-function showCalendar(){
-  var calendarEl = document.getElementById("calendar");
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "timeGridWeek",
-    themeSystem: 'bootstrap5',
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'timeGridWeek,timeGridDay,listMonth'
-    },
-    droppable: false,
-    allDaySlot: false,
-    slotDuration: "01:00",
-    slotMinTime: "08:00:00",
-    slotMaxTime: "17:00:00",
-    height: "auto",
-    locale: "local",
-    timeZone: "UTC",
-    titleFormat: {
-      year: 'numeric',
-      month: 'short',
-      year: 'numeric',
-      day: 'numeric'
-    },
-    dateClick: function (info) {
-      if (!isEventSelected) {
-        var date = new Date(info.start);
-        var confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
-        document.getElementById('confirmButton').addEventListener('click', function () {
-          info.backgroundColor = "#ff0066";
-          info.borderColor = 'transparent'
-          calendar.addEvent(info);
-          confirmationModal.hide();
-          isEventSelected = true; // evento selecionado pelo usuário nesse imóvel
+// Elements
+let visitsForFullCalendar = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    API.get("properties/" + id)
+        .then(response => response.json()) 
+        .then(visitsJson => {
+            if (visitsJson == null) {
+                var calendarEl = document.getElementById('calendar');
+
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    locale: 'pt-br',
+                    initialView: 'dayGridMonth',
+                    headerToolbar: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    }
+                });
+                for (const event of visitsForFullCalendar) {
+                    calendar.addEvent(event);
+                }
+                calendar.render();
+            } else {
+                visitsJson.forEach(visita => {
+                    visitsForFullCalendar.push({
+                        start: new Date(visita.createdAt[0], visita.createdAt[1] - 1, visita.createdAt[2],
+                            visita.createdAt[3], visita.createdAt[4], visita.createdAt[5])
+                    });
+                });
+
+                var calendarEl = document.getElementById('calendar');
+
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    locale: 'pt-br',
+                    initialView: 'dayGridMonth',
+                    headerToolbar: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    }
+                });
+
+                for (const event of visitsForFullCalendar) {
+                    calendar.addEvent(event);
+                }
+                calendar.render();
+            }
+        })
+        .catch(error => {
+            console.log(error);
         });
-        confirmationModal.show();
-      }else{
-        alert("Você já selecionou um evento nesse imóvel. Para selecionar um novo evento, cancele o evento existente.");
-      }
-    },
-    selectable: true,
-    events: [ //todo: API aqui
-      {
-        start: '2023-10-27T13:00:00Z',
-        backgroundColor: 'gray',
-        borderColor: 'transparent'
-      },
-      {
-        start: '2023-10-27T10:00:00Z',
-        backgroundColor: 'gray',
-        borderColor: 'transparent'
-      },
-      {
-        start: '2023-10-29T09:00:00Z',
-        backgroundColor: 'gray',
-        borderColor: 'transparent'
-      },
-      {
-        start: '2023-10-30T15:00:00Z',
-        backgroundColor: 'gray',
-        borderColor: 'transparent'
-      },
-      {
-        start: '2023-11-01T09:00:00Z',
-        backgroundColor: 'gray',
-        borderColor: 'transparent'
-      },
-      {
-        start: '2023-11-01T10:00:00Z',
-        backgroundColor: 'gray',
-        borderColor: 'transparent'
-      },
-      {
-        start: '2023-11-01T13:00:00Z',
-        backgroundColor: 'gray',
-        borderColor: 'transparent'
-      }
-    ],
-  });
-  calendar.on('select', function(info) {
-    console.log(info);
-    console.log("Início: "+ info.start);
-    console.log("InícioStr: "+ info.startStr);    
 });
-calendar.render();
-}
-
-
-
-
-
