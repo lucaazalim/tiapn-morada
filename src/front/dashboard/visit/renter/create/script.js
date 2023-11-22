2a7856566cb2cadd5c243b7f7fbbbc56c05ed407
Kimberly Spencer <kimberly.liz@sga.pucminas.br>
2023-11-22 17:57:23 -0300

    vf. visits view

--- src/front/dashboard/visit/renter/create/script.js
+++ src/front/dashboard/visit/renter/create/script.js
@@ -2,7 +2,6 @@ import * as API from '../../../../assets/script/api.js';
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
-let event = [];
 
 let visitsForFullCalendar = [];
 
@@ -37,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 visits.forEach(visit => {
                     let data = new Date(visit.datetime[0], visit.datetime[1]-1, visit.datetime[2],
                         visit.datetime[3], visit.datetime[4]);
-                    
+                    //console.log(new Date(data.toUTCString())) //todo: para análises
                     visitsForFullCalendar.push({
                         start: new Date(data.toUTCString()) 
                     })
@@ -45,23 +44,22 @@ document.addEventListener('DOMContentLoaded', function () {
                     var calendarEl = document.getElementById('calendar');
 
                     var calendar = new FullCalendar.Calendar(calendarEl, {
-                    locale: 'pt-br',
-                    initialView: "timeGridWeek",
-                    headerToolbar: {
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                    },
-                    allDaySlot: false,
-                    slotDuration: "01:00",
-                    slotMinTime: "08:00:00",
-                    slotMaxTime: "17:00:00",
-                    height: "auto"
-
+                        locale: 'pt-br',
+                        initialView: "timeGridWeek",
+                        headerToolbar: {
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                        },
+                        allDaySlot: false,
+                        slotDuration: "01:00",
+                        slotMinTime: "08:00:00",
+                        slotMaxTime: "17:00:00",
+                        height: "auto"
                     });
-                    console.log(event);
                     for (const event of visitsForFullCalendar) {
                         calendar.addEvent(event);
+                        //console.log(event); //todo: para análises
                     }
                     calendar.render();
                 
