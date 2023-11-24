0c3d8f3a83a5a63aaeb559851d54d11d2cbf75b0
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-24 15:16:15 -0300

    Removendo imports sem uso. #48

--- src/back/src/main/java/br/pucminas/morada/services/PaymentService.java
+++ src/back/src/main/java/br/pucminas/morada/services/PaymentService.java
@@ -1,7 +1,6 @@
 package br.pucminas.morada.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
