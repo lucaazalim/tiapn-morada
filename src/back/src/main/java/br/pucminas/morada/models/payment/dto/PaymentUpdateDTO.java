fdb97c798c5b7cb41f5427861395f99f5069788c
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-24 15:16:12 -0300

    Removendo imports sem uso. #48

--- src/back/src/main/java/br/pucminas/morada/models/payment/dto/PaymentUpdateDTO.java
+++ src/back/src/main/java/br/pucminas/morada/models/payment/dto/PaymentUpdateDTO.java
@@ -1,11 +1,9 @@
 package br.pucminas.morada.models.payment.dto;
 
-import br.pucminas.morada.models.payment.Payment;
 import br.pucminas.morada.models.payment.PaymentStatus;
 import lombok.Data;
 import org.springframework.lang.Nullable;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Data
 public class PaymentUpdateDTO {
