91b4c9e8cfeb9843f56ff8c7f597634e04553749
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-26 08:27:30 -0300

    Update PaymentController #48

--- src/back/src/main/java/br/pucminas/morada/controllers/PaymentController.java
+++ src/back/src/main/java/br/pucminas/morada/controllers/PaymentController.java
@@ -1,24 +1,20 @@
 package br.pucminas.morada.controllers;
 
-import br.pucminas.morada.models.payment.PaymentStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.pucminas.morada.models.payment.Payment;
 import br.pucminas.morada.models.payment.dto.PaymentCreateDTO;
-import br.pucminas.morada.models.payment.dto.PaymentDTO;
 import br.pucminas.morada.models.payment.dto.PaymentUpdateDTO;
-import br.pucminas.morada.models.rental.Rental;
 import br.pucminas.morada.services.PaymentService;
-import br.pucminas.morada.services.RentalService;
 import jakarta.validation.Valid;
 
-import java.time.LocalDateTime;
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/payments")
@@ -26,52 +22,41 @@ import java.util.stream.Collectors;
 public class PaymentController {
 
     @Autowired
-    private RentalService rentalService;
-
-    @Autowired
     private PaymentService paymentService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PaymentDTO> findById(@PathVariable Long id) {
-        Payment payment = paymentService.findById(id);
-        return ResponseEntity.ok(payment.toDTO());
-    }
-
-    @GetMapping
-    public ResponseEntity<List<PaymentDTO>> findAll() {
-        List<Payment> payments = paymentService.findAll();
-        List<PaymentDTO> paymentDTOs = payments.stream()
-                .map(Payment::toDTO)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(paymentDTOs);
-    }
-
     @PostMapping
-    public ResponseEntity<PaymentDTO> create(@Valid @RequestBody PaymentCreateDTO paymentCreateDTO) {
-        Payment payment = paymentService.save(convertDto(paymentCreateDTO));
-        return new ResponseEntity<>(payment.toDTO(), HttpStatus.CREATED);
-    }
+    public ResponseEntity<Void> create(@Valid @RequestBody PaymentCreateDTO paymentCreateDTO) {
+        Payment payment = paymentCreateDTO.toEntity(Payment.class);
+        paymentService.create(payment, paymentCreateDTO.rentalId()); // Assuming create method similar to offerService
 
-    private Payment convertDto(PaymentCreateDTO dto) {
-        Rental rental = rentalService.findById(dto.getRentalId());
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(payment.getId())
+                .toUri();
 
-        PaymentStatus status = PaymentStatus.valueOf(dto.getStatus());
-
-        return Payment.builder()
-                .rental(rental)
-                .rentValue(dto.getRentValue())
-                .competenceMonth(dto.getCompetenceMonth())
-                .competenceYear(dto.getCompetenceYear())
-                .status(status)
-                .createdAt(LocalDateTime.now())
-                .build();
+        return ResponseEntity.created(uri).build();
     }
 
     @PutMapping("/{id}")
+    @Validated
     public ResponseEntity<Void> update(
-            @Valid @RequestBody PaymentUpdateDTO paymentUpdateDTO, @PathVariable Long id) {
-        paymentService.update(id, paymentUpdateDTO);
+            @Valid @RequestBody PaymentUpdateDTO paymentUpdateDTO,
+            @PathVariable Long id
+    ) {
+        paymentService.update(id, paymentUpdateDTO.status());
         return ResponseEntity.noContent().build();
     }
-}
 
+    @GetMapping("/user")
+    public ResponseEntity<List<Map<String, Object>>> findAllPayments() {
+        List<Map<String, Object>> payments = paymentService.findAllPaymentsForTheUser();
+        return ResponseEntity.ok(payments);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Payment> findById(@PathVariable Long id) {
+        Payment payment = paymentService.findById(id);
+        return ResponseEntity.ok(payment);
+    }
+
+}
