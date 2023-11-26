99d352a44121d6c6250e02846c7b5c449caec194
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-26 08:27:50 -0300

    Update PaymentService #48

--- src/back/src/main/java/br/pucminas/morada/services/PaymentService.java
+++ src/back/src/main/java/br/pucminas/morada/services/PaymentService.java
@@ -5,13 +5,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.pucminas.morada.models.payment.Payment;
-import br.pucminas.morada.models.payment.dto.PaymentUpdateDTO;
+import br.pucminas.morada.models.payment.PaymentStatus;
+import br.pucminas.morada.models.rental.Rental;
+import br.pucminas.morada.models.user.User;
+import br.pucminas.morada.models.user.UserRole;
 import br.pucminas.morada.repositories.PaymentRepository;
-import jakarta.validation.Valid;
+import br.pucminas.morada.security.UserSpringSecurity;
+import br.pucminas.morada.services.exceptions.AuthorizationException;
+import br.pucminas.morada.services.exceptions.GenericException;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
+
 
 @Service
 public class PaymentService {
@@ -19,39 +26,61 @@ public class PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
 
-    public Payment findById(Long id) {
-        Optional<Payment> optionalPayment = paymentRepository.findById(id);
+    @Autowired
+    private RentalService rentalService;
 
-        if (optionalPayment.isPresent()) {
-            return optionalPayment.get();
-        } else {
-            throw new RuntimeException("Pagamento não encontrado.");
-        }
-    }
-
-    public List<Payment> findAll() {
-        return paymentRepository.findAll();
-    }
-
-    public Payment save(Payment payment) {
-       
-        if (payment.getCreatedAt() == null) {
-            payment.setCreatedAt(LocalDateTime.now());
-        }
-    
-        return paymentRepository.save(payment);
-    }
+    @Autowired
+    private UserService userService;
 
     @Transactional
-    public Payment update(Long id, @Valid PaymentUpdateDTO paymentUpdateDTO) {
-        Payment payment = findById(id);
+    public Payment create(Payment payment, Long rentalId) {
+        Rental rental = this.rentalService.findById(rentalId);
+        User user = this.userService.findById(UserService.getAuthenticatedUser().getId());
 
-            payment.setRentValue(paymentUpdateDTO.getRentValue());
-            payment.setCompetenceMonth(paymentUpdateDTO.getCompetenceMonth());
-            payment.setCompetenceYear(paymentUpdateDTO.getCompetenceYear());
-            payment.setStatus(paymentUpdateDTO.getStatus());
-            //payment.setCreatedAt(paymentUpdateDTO.getCreatedAt());
-        return paymentRepository.save(payment);
+        payment.setRental(rental);
+        payment.setUser(user);
+
+        return this.paymentRepository.save(payment);
     }
+
+
+    @Transactional
+    public Payment update(Long id, PaymentStatus status) {
+        Payment paymentFound = this.findById(id);
+        paymentFound.setStatus(status);
+
+        return this.paymentRepository.save(paymentFound);
+    }
+
+    public Payment findById(Long id) {
+    Optional<Payment> optionalPayment = this.paymentRepository.findById(id);
+    UserSpringSecurity userSpringSecurity = UserService.getAuthenticatedUser();
+
+    if (optionalPayment.isEmpty()) {
+        if (userSpringSecurity != null && userSpringSecurity.hasRole(UserRole.ADMIN)) {
+            throw new GenericException(HttpStatus.NOT_FOUND, "Pagamento não encontrado");
+        }
+        throw new AuthorizationException();
+    } else {
+        Payment payment = optionalPayment.get();
+        User user = payment.getRental().getUser();
+        if (userSpringSecurity != null && (userSpringSecurity.hasRole(UserRole.ADMIN)
+                || user.getId().equals(userSpringSecurity.getId()))) {
+            return payment;
+        }
+    }
+    throw new AuthorizationException();
 }
 
+
+    public List<Payment> findAllByUser() {
+        UserSpringSecurity userSpringSecurity = UserService.getAuthenticatedUser();
+        return this.paymentRepository.findByUser_Id(userSpringSecurity.getId());
+    }
+
+    public List<Map<String, Object>> findAllPaymentsForTheUser() {
+        UserSpringSecurity userSpringSecurity = UserService.getAuthenticatedUser();
+        return this.paymentRepository.findAllPaymentsByUserId(userSpringSecurity.getId());
+    }
+
+}
