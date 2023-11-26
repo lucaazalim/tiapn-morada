45a2775b88445d1ad514a1ef208678ac6842dd6e
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-26 08:26:54 -0300

    Update PaymentDTO #48

--- src/back/src/main/java/br/pucminas/morada/models/payment/dto/PaymentUpdateDTO.java
+++ src/back/src/main/java/br/pucminas/morada/models/payment/dto/PaymentUpdateDTO.java
@@ -1,16 +1,15 @@
 package br.pucminas.morada.models.payment.dto;
 
+import br.pucminas.morada.models.DTO;
+import br.pucminas.morada.models.payment.Payment;
 import br.pucminas.morada.models.payment.PaymentStatus;
-import lombok.Data;
-import org.springframework.lang.Nullable;
-import java.math.BigDecimal;
+import jakarta.validation.constraints.NotNull;
 
-@Data
-public class PaymentUpdateDTO {
-    private Long id;
-    @Nullable private BigDecimal rentValue;
-    @Nullable private Integer competenceMonth;
-    @Nullable private Integer competenceYear;
-    @Nullable private PaymentStatus status;
+public record PaymentUpdateDTO(
+
+        @NotNull PaymentStatus status
+        
+) implements DTO<Payment> {
 }
+
 
