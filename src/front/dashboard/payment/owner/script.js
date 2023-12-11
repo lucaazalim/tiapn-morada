c116771317e6437fdb5f665874a38854d0b487f0
Luca Azalim <lucaazalim@gmail.com>
2023-12-11 17:16:55 -0300

    Ajustes no processo de pagamento.

--- src/front/dashboard/payment/owner/script.js
+++ src/front/dashboard/payment/owner/script.js
@@ -1,12 +1,13 @@
 import * as API from "../../../assets/script/api.js";
+import * as Alert from '../../../assets/script/alert.js';
 
 let paymentsReceived = document.getElementById("paymentsReceived");
 
-window.updatePaymentStatus = function (paymentId, newStatus) {
+window.updatePaymentStatus = function (paymentId, status) {
 
-  API.put(`payments/${paymentId}`, { status: PaymentStatus[newStatus] })
+  API.put(`payments/${paymentId}`, { status })
     .then(() => {
-      alert(`Status atualizado para: ${newStatus}`);
+      Alert.alert(`Status atualizado para: ${API.paymentStatusMap[status].label}!`, 'success');
       loadPayments();
     })
     .catch(error => {
@@ -20,16 +21,28 @@ function loadPayments() {
     .then(payments => {
       paymentsReceived.innerHTML = '';
       payments.forEach(payment => {
+
+        let buttons = payment.status === 'ALLEGEDLY_PAID' ? `
+          <button class="btn btn-success" onclick="updatePaymentStatus(${payment.id}, 'CONFIRMED')">Confirmar</button>
+          <button class="btn btn-danger" onclick="updatePaymentStatus(${payment.id}, 'REJECTED')">Rejeitar</button>
+        ` : '';
+
         paymentsReceived.innerHTML += `
           <div class="card mb-3">
             <div class="card-header">
               Competência do Pagamento: ${payment.competenceMonth}/${payment.competenceYear}
             </div>
             <div class="card-body">
-              <h5 class="card-title">Valor: R$ ${payment.rentValue.toFixed(2)}</h5>
+              <div class="d-flex justify-content-between">
+                <h5 class="card-title">
+                  Valor: R$ ${payment.rentValue.toFixed(2)}
+                </h5>
+                <h5 class="card-title text-right">
+                  Aluguel: ${payment.rentalId}
+                </h5>
+              </div>
               <p class="card-text">Status: ${API.paymentStatusMap[payment.status].label}</p>
-              <button class="btn btn-success" onclick="updatePaymentStatus(${payment.id}, 'CONFIRMED')">Confirmar</button>
-              <button class="btn btn-danger" onclick="updatePaymentStatus(${payment.id}, 'REJECTED')">Rejeitar</button>
+              ${buttons}
             </div>
           </div>
         `;
@@ -42,8 +55,3 @@ function loadPayments() {
 }
 
 loadPayments();
-
-const PaymentStatus = {
-  CONFIRMED: 'CONFIRMED',
-  REJECTED: 'REJECTED'
-};
